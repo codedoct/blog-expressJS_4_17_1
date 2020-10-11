58e310eebe44858baab41bfb2ede06030727ce90
victra <victra.sonata@gmail.com>
2020-10-11 16:31:15 +0700

    crud read feature news

--- controllers/v1/newsController.js
+++ controllers/v1/newsController.js
@@ -3,6 +3,7 @@ const News = require('../../db/models/news')
 const errorHandler = require('../../services/error')
 const newsTransform = require('../../transformers/news-transform')
 const formater = require('../../utils/formater')
+const constant = require('../../utils/constant')
 const validator = require('../../validators/news')
 
 app.post('/', async (req,res) => {
@@ -10,7 +11,7 @@ app.post('/', async (req,res) => {
         const validate = validator.createUpdateNews.validate(req.body)
         if (validate.error) throw validate.error
 
-        const newsExists = await News.findOne({title: req.body.title})
+        const newsExists = await News.findOneByTitle(req.body.title)
         if (newsExists) return errorHandler.BadRequest(res, "News is already exists.")
 
         if (!req.body.status) delete req.body.status
@@ -23,6 +24,45 @@ app.post('/', async (req,res) => {
     } catch (err) {
         errorHandler.UnHandler(res, err)
     }
-});
+})
+
+app.get('/', async (req,res) => {
+    try {
+        const meta = {
+            ...constant.dataMeta(req),
+            title: req.query.title == undefined ? "" : req.query.title
+        }
+        
+        let sort = {created_at: -1}
+        let query = {title: new RegExp(meta.title, "i")}
+
+        const options = {
+            lean: false,
+            sort: sort,
+            page: meta.page,
+            limit: meta.limit
+        }
+        
+        const result = await News.paginateNews(query, options)
+        result.docs = newsTransform.listNews(result.docs)
+        result.meta = meta
+
+        res.json({ status: "OK", result })
+    } catch (err) {
+        errorHandler.UnHandler(res, err)
+    }
+})
+
+app.get('/:news_id', async (req,res) => {
+    try {
+        const newsDetail = await News.findOneByID(req.params.news_id)
+        const result = newsTransform.showNews(newsDetail)
+
+        res.json({ status: "OK", result })
+    } catch (err) {
+        errorHandler.UnHandler(res, err)
+    }
+})
+
 
 module.exports = app
--- db/models/news.js
+++ db/models/news.js
@@ -1,10 +1,23 @@
 const newsSchema = require('../schemas/newsSchema')
 const mongoose = require('mongoose')
+const mongoosePaginate = require('mongoose-paginate-v2');
+newsSchema.plugin(mongoosePaginate);
 
 const News = mongoose.model('news', newsSchema)
-
 News.createNews = (newNews) => {
     return newNews.save()
+}
+
+News.paginateNews = (query, options) => {
+    return News.paginate(query, options)
+}
+
+News.findOneByID = (newsID) => {
+    return News.findById(newsID)
+}
+
+News.findOneByTitle = (newsTitle) => {
+    return News.findOne({title: newsTitle})
 }
 
 module.exports = News
